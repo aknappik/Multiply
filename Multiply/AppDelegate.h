e2a3c1b206e5cda98c82fb13c22d07da7f0e1944
Alison Knappik <alison.knappik@d214.org>
2014-05-12 15:13:22 -0500

    Initial Commit

---
+++ Multiply/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Multiply
+//
+//  Created by Alison K. Knappik on 5/12/14.
+//  Copyright (c) 2014 aknapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
