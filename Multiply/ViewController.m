acad6ff212fe960cc12b846df0d57fcb7b0578fa
Alison Knappik <alison.knappik@d214.org>
2014-05-13 13:44:09 -0500

    This is my first big commit!

--- Multiply/ViewController.m
+++ Multiply/ViewController.m
@@ -9,15 +9,64 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *myNumber;
+@property (weak, nonatomic) IBOutlet UILabel *myMultiplier;
+@property (weak, nonatomic) IBOutlet UILabel *myAnswer;
+@property (weak, nonatomic) IBOutlet UISlider *mySlider;
+
+
 
 @end
 
 @implementation ViewController
 
+int sliderValue;
+
+- (IBAction)sliderAction:(id)sender {
+    sliderValue = (int)self.mySlider.value;
+    self.myNumber.text = [NSString stringWithFormat:@"%d", sliderValue];
+    
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+   // UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
+
+   
+    
+}
+
+
+
+- (IBAction)onCalculateButtonPressed:(id)sender {
+    NSInteger myNumber = [self.myNumber.text integerValue];
+    NSInteger myMultiplier = [self.myMultiplier.text integerValue];
+    NSInteger finalValue = myNumber * myMultiplier;
+    NSString *stringValue = [NSString stringWithFormat:@"%d", finalValue];
+    
+    
+    if (finalValue > 20) {
+        self.view.backgroundColor = [UIColor greenColor];
+    } else {
+        self.view.backgroundColor = [UIColor whiteColor];
+    }
+    
+    if ((finalValue %3 == 0) && (finalValue %5 == 0)){
+        self.myAnswer.text = @"fizzbuzz";
+    }
+        else if (finalValue %3 == 0) {
+        self.myAnswer.text = @"fizz";}
+    
+            else if (finalValue %5 ==0) {
+            self.myAnswer.text = @"buzz";
+            }else {
+                self.myAnswer.text = stringValue;
+            }
+    //gets rid of the keyboard
+    [self.myAnswer resignFirstResponder];
+
 }
 
 - (void)didReceiveMemoryWarning
